c9a80f4154009a033e7abd12b1064059fbb9673f
formula38 <louismconley@gmail.com>
2024-04-05 12:24:42 -0700

    + Connected Serverside to Forntend for Contract form - Completed Services & Form - Need Template

--- rolclient/src/app/about/mission-vision/mission-vision.component.ts
+++ rolclient/src/app/about/mission-vision/mission-vision.component.ts
@@ -1,84 +1,10 @@
-import {Component, OnInit} from '@angular/core';
-import {ContactFormEntryService} from "../../api/services/contact-form-entry.service";
-import {ActivatedRoute, Router} from "@angular/router";
-import {FormControl, FormGroup} from "@angular/forms";
-import {tap} from "rxjs";
-import {ContactFormEntry} from "../../api/interfaces/contact-form-entry";
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-mission-vision',
   templateUrl: './mission-vision.component.html',
   styleUrls: ['./mission-vision.component.css']
 })
-export class MissionVisionComponent implements OnInit{
-
-  onCancel() {
-    this.router.navigate(
-      ['../'],
-      {relativeTo: this.route}
-    );
-  }
-
-  onSubmit() {
-    if (this.editMode) {
-      this.contactFromEntryService.updateEntry(this.contactForm.value)
-        .pipe(tap(entry => {
-          this.contractFormEntry = entry;
-        })).subscribe();
-    } else {
-      this.contactFromEntryService.createNewEntry(this.contactForm.value)
-        .pipe(tap(newEntry => {
-          this.contractFormEntry = newEntry;
-        })).subscribe();
-    }
-    this.onCancel();
-  }
-
-  private async initForm() {
-    let entryId: number;
-    let firstName: string;
-    let lastName: string;
-    let email: string;
-    let message: string;
-
-    if (this.editMode) {
-      const entry = await this.contactFromEntryService
-        .getByEntryId(this.id).toPromise();
-
-      entryId = entry.entryId;
-      firstName = entry.firstName;
-      lastName = entry.lastName;
-      email = entry.email;
-      message = entry.message;
-
-    }
-
-    this.contactForm = new FormGroup<any>({
-      entryId: new FormControl(entryId),
-      firstName: new FormControl(firstName),
-      lastName: new FormControl(lastName),
-      email: new FormControl(email),
-      message: new FormControl(message)
-    })
-  }
-
-
-  constructor(private contactFromEntryService: ContactFormEntryService,
-              private route: ActivatedRoute,
-              private router: Router) {
-  }
-  contractFormEntry: ContactFormEntry;
-  contactForm: FormGroup;
-  id: number;
-  editMode = false;
-
-  ngOnInit(): void {
-    this.route.params
-      .subscribe(param => {
-        this.id = param['id'];
-        this.editMode = param['id'] != null;
-        this.initForm();
-      })
-  }
+export class MissionVisionComponent {
 
 }
--- rolclient/src/app/forms/contact-form/contact-form.component.ts
+++ rolclient/src/app/forms/contact-form/contact-form.component.ts
@@ -1,10 +1,83 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {tap} from "rxjs";
+import {FormControl, FormGroup} from "@angular/forms";
+import {ContactFormEntryService} from "../../api/services/contact-form-entry.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {ContactFormEntry} from "../../api/interfaces/contact-form-entry";
 
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.css']
 })
-export class ContactFormComponent {
+export class ContactFormComponent implements OnInit{
+
+  onCancel() {
+    this.router.navigate(
+      ['../'],
+      {relativeTo: this.route}
+    );
+  }
+
+  onSubmit() {
+    if (this.editMode) {
+      this.contactFromEntryService.updateEntry(this.contactForm.value)
+        .pipe(tap(entry => {
+          this.contractFormEntry = entry;
+        })).subscribe();
+    } else {
+      this.contactFromEntryService.createNewEntry(this.contactForm.value)
+        .pipe(tap(newEntry => {
+          this.contractFormEntry = newEntry;
+        })).subscribe();
+    }
+    this.onCancel();
+  }
+
+  private async initForm() {
+    let entryId: number;
+    let firstName: string;
+    let lastName: string;
+    let email: string;
+    let message: string;
+
+    if (this.editMode) {
+      const entry = await this.contactFromEntryService
+        .getByEntryId(this.id).toPromise();
+
+      entryId = entry.entryId;
+      firstName = entry.firstName;
+      lastName = entry.lastName;
+      email = entry.email;
+      message = entry.message;
+
+    }
+
+    this.contactForm = new FormGroup<any>({
+      entryId: new FormControl(entryId),
+      firstName: new FormControl(firstName),
+      lastName: new FormControl(lastName),
+      email: new FormControl(email),
+      message: new FormControl(message)
+    })
+  }
+
+  constructor(private contactFromEntryService: ContactFormEntryService,
+              private route: ActivatedRoute,
+              private router: Router) {
+  }
+  contractFormEntry: ContactFormEntry;
+  contactForm: FormGroup;
+  id: number;
+  editMode = false;
+
+  ngOnInit(): void {
+    this.route.params
+      .subscribe(param => {
+        this.id = param['id'];
+        this.editMode = param['id'] != null;
+        this.initForm();
+      })
+  }
 
 }
